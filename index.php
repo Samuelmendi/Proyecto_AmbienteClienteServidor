65e9eaf4336dff254fc810289aa158880bffc328
Samuelmendi <samytorresm@gmail.com>
2025-03-08 15:28:16 -0600

    Pagina de inicio barebones
    Estructura inicial de la pagina de inicio

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head> 
+        <title>MediCare</title>
+        <link rel="stylesheet" href="css/stylesheet.css">
+    </head>
+
+    <body>
+        <div id = "WebHeader">
+            <h1>Medicare</h1>
+            <div id = "loginout">
+                <a href="Login-registro/Log-in.php" id="Login">Log-in</a>
+                <a href="Login-registro/registro.php" id="Registro">Registro</a>
+            </div>
+        </div>
+        <h2>Bienvenidos</h2>
+        <div>
+            <h2>Caracteristicas principales</h2>
+            <div id="Caracteristicas">
+                <ul>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                </ul>
+                <ul>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <h2>Sobre Nosotros</h2>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            </div>
+        </div>
+        <footer>
+            <p>Footer</p>
+        </footer>
+
+    </body>
+</html>
